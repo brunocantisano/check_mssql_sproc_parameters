89791d8e362eecd6a07991312657568f559899ab
Bruno Cardoso Cantisano <bruno.cantisano@gmail.com>
2018-05-04 09:59:54 -0500

    Fixing html. It's not using anymore a file to assemble the html.

--- check_mssql_sproc_parameters.pl
+++ check_mssql_sproc_parameters.pl
@@ -14,6 +14,8 @@
 # AUTHOR: Bruno Cardoso Cantisano <bruno.cantisano@gmail.com>
 # DATE  : 04/25/2017
 # DATE  : 12/14/2017: Adding custom html page
+# DATE  : 05/04/2018: Fixing html. It's not using anymore a file to 
+# assemble the html.
 #
 # PURPOSE: Updated to allow input parameters and format the results in HTML.
 # ======================================================================
@@ -31,15 +33,15 @@ $sql_pass = "";
 my ($opt_h, $opt_proc, $opt_host, $opt_user, $opt_pw, $opt_c, $opt_w, $opt_ver);
 Getopt::Long::Configure('bundling');
 GetOptions(
-	   "h"   => \$opt_h, "help"  => \$opt_h,
-	   "p=s"   => \$opt_proc,  "procedure=s"  => \$opt_proc,
-	   "H=s"   => \$opt_host,  "hostname=s"  => \$opt_host,
-	   "d=s"   => \$opt_db,	   "database=s"  => \$opt_db,
-	   "u=s"   => \$opt_user,  "user=s"  => \$opt_user,
-	   "P=s"   => \$opt_pw,    "password=s"  => \$opt_pw,
-	   "c=s"   => \$opt_c,     "critical=s"  => \$opt_c,
-	   "w=s"   => \$opt_w,     "warning=s"  => \$opt_w,
-	   "v"     => \$opt_ver,    "version"  => \$opt_ver,
+           "h"   => \$opt_h, "help"  => \$opt_h,
+           "p=s"   => \$opt_proc,  "procedure=s"  => \$opt_proc,
+           "H=s"   => \$opt_host,  "hostname=s"  => \$opt_host,
+           "d=s"   => \$opt_db,    "database=s"  => \$opt_db,
+           "u=s"   => \$opt_user,  "user=s"  => \$opt_user,
+           "P=s"   => \$opt_pw,    "password=s"  => \$opt_pw,
+           "c=s"   => \$opt_c,     "critical=s"  => \$opt_c,
+           "w=s"   => \$opt_w,     "warning=s"  => \$opt_w,
+           "v"     => \$opt_ver,    "version"  => \$opt_ver,
 );
 
 if ($opt_h) {
@@ -48,14 +50,14 @@ if ($opt_h) {
 }
 
 if ($opt_ver) {
-	print_version();
-	exit;
+        print_version();
+        exit;
 }
 
 if ($hardcoded) {
-	$opt_user = $sql_user;
-	$opt_pw = $sql_pass;
-	}
+        $opt_user = $sql_user;
+        $opt_pw = $sql_pass;
+        }
 
 if ($opt_host eq "" or $opt_proc eq "" or $opt_db eq "" or $opt_user eq "" or $opt_pw eq "" or $opt_c eq "" or $opt_w eq "") {
 print "ERROR: Mandatory arguments -H, -u, -P, -d, -p, -c and -w are required.\n";
@@ -68,20 +70,20 @@ my $conn;
 $conn{"username"} = $opt_user;
 $conn{"server"} = $opt_host;
 $conn{"password"} = $opt_pw;
-$conn{"dsn"} = "dbi:ODBC:Driver={ODBC Driver 17 for SQL Server};SERVER=" . $conn{"server"};
+$conn{"dsn"} = "dbi:ODBC:Driver={ODBC Driver 11 for SQL Server};SERVER=" . $conn{"server"};
 $conn{"dbh"} = DBI-> connect( $conn{"dsn"}, $conn{"username"}, $conn{"password"})
-	or die "Error: Unable to connect to MS-SQL database!\n", $DBI::errstr,"\n";
+        or die "Error: Unable to connect to MS-SQL database!\n", $DBI::errstr,"\n";
 
 $nomeProc =  substr $opt_proc, 0, index($opt_proc, ' ');
-$paramsProc = join("", split(/[aA-zZ]|[0-9]|\'/, $opt_proc, -1));
+$paramsProc = join("", split(/[aA-zZ]|[0-9]|"/, $opt_proc, -1));
 $paramsProc = substr $paramsProc, 2, length($paramsProc)-2;
 $paramsProc =~ s/ /?/g;
 
 my $sql = qq|
-			begin
-				use $opt_db
-				exec $opt_proc
-			end|;
+        begin
+                use $opt_db
+                exec $opt_proc
+        end|;
 
 
 my $sql = qq{ use $opt_db exec $opt_proc };
@@ -93,108 +95,106 @@ $sth->{"LongTruncOk"} = 1;
 $sth->execute();
 
 $temHeader = 1;
-$header = "";
-$table = "";
-$footer = "";
+$table = '<script>
+           $(function(){    
+                $("#toggle").click(function(){
+                    if($("#' . $opt_proc . '").find("tbody").is(":visible") == true){
+                      $("#' . $opt_proc . '").find("tbody").hide();
+                      $("#toggle").val("+");
+                    }
+                    else{   
+                      $("#' . $opt_proc . '").find("tbody").show();
+                      $("#toggle").val("-");
+                    }
+                });
+                $("#' . $opt_proc . '").find("tbody").hide();
+           });
+          </script>
+          <table class="table table-striped" id="' . $opt_proc . '">
+           <thead>
+                <tr>
+                    <td>
+                        <input id="toggle" type="checkbox">
+                        <label id="primary-links" for="toggle">
+                            <div class="label_##LABEL##"></div>
+                            <span class="info">##TYPE##: SQL Query returned ' . $results . ' for stored procedure ' . $opt_proc . '</span>
+                        </label>
+                    </td>
+                </tr>
+           </thead>
+           <tbody>';
+
 my $ref;
 my $results = "";
 
-
 $i = 1;
 while($ref = $sth->fetchrow_hashref) {
-	if($temHeader){
-		$header .= '<div class="box-row header"><table><tr>';
-		foreach my $field ( keys %{ $ref } ) {
-			if($field ne 'retorno'){
-				$header = $header . '<td>' . $field . '</td>';
-			}
-		}
-		$header .= '</tr></table></div>';
-		$temHeader = 0;
-	}
-	if($i > 0){ #para não considerar a primeira coluna porque contém o código de retorno
-		$table .= '<div class="box-row linha"><table><tr>';
-	}
+        if($temHeader){
+            $temHeader = 0;
+        }
+        if($i > 0){ #para não considerar a primeira coluna porque contém o código de retorno
+            $table .= '<tr>';        
+        }
 
-	foreach my $field ( keys %{ $ref } ) {
-		if($field eq 'retorno'){
-			$results = $ref->{ $field };
-		}
-		else{
-			$table .= '<td>' . $ref->{ $field } . '</td>';
-		}
-	}
+        foreach my $field ( keys %{ $ref } ) {
+            if($field eq 'retorno'){
+                    $results = $ref->{ $field };
+            }
+            else{
+                    $table .= '<td>' . $ref->{ $field } . '</td>';
+            }
+        }
 
-	if($i > 0){ #para não considerar a primeira coluna porque contém o código de retorno
-		$table .= '</tr></table></div>';
-	}
+        if($i > 0){ #para não considerar a primeira coluna porque contém o código de retorno
+            $table .= '</tr>';
+        }
 
-	$i = $i + 1;
+        $i = $i + 1;
 }
-
-my $indexFile = 
-"<link rel=\"stylesheet\" type=\"text/css\" href=\"./stylesheets/style.css\">
-<main>
-  <input id=\"toggle\" type=\"checkbox\">
-  <label id=\"primary-links\" for=\"toggle\">
-  <div class=\"label_##TYPE##\"></div><span class=\"info\">##TYPE## - ##MESSAGE##</span>
-  </label>
-  <div id=\"expand\">
-    <section>
-		<div id=\"squeeze\">
-			<div class=\"content\">						
-				<div id=\"tabela\"><a href=\"/tabela/\" target=\"_blank\"></a>
-					<div id=\"cp_toggle\">										
-						<div class=\"boxer\">							
-			                	##CONTENT##
-						</div>
-					</div>
-				</div>
-			</div>		
-		</div>
-    </section>
-  </div> 
-</main>";
-
-process_results($results, $indexFile);
+$table .= '</tbody></table>';
+process_results($results);
 
 $sth->finish();
 $conn{"dbh"}->disconnect();
 
 sub process_results {
-	if($results >= $opt_c) {
-		print_critical($results, $indexFile);
-	}
-	elsif($results >= $opt_w) {
-		print_warning($results, $indexFile);
-	}
-	else {		
-		$footer = '<div class="box-row footer"><table><tr><td>OK: Returned values for stored procedure $opt_proc</td></tr></table></div>';
-		$indexFile =~ s/##CONTENT##/$header$table$footer/g;
-		$indexFile =~ s/##TYPE##/ok/g;
-		$indexFile =~ s/##MESSAGE##/OK: Returned values for stored procedure $opt_proc/g;
-		print $indexFile;
-		exit 0;
-	}
+    #removing breaklines, tabs and two or more spaces
+    $table =~ s/\n|\r|\t|\s{2,}//g;
+    if($results >= $opt_c) {
+        print_critical($results);
+    }
+    elsif($results >= $opt_w) {
+        print_warning($results);
+    }
+    else {
+        print_ok($results); 
+    }
 }
 
-sub print_warning($results, $indexFile) {
-	$footer = '<div class="box-row footer"><table><tr><td>WARNING: SQL Query returned $results. for stored procedure $opt_proc. The warning threshold is $opt_w.</td></tr></table></div>';
-	$indexFile =~ s/##CONTENT##/$header$table$footer/g;
-	$indexFile =~ s/##TYPE##/aviso/g;
-	$indexFile =~ s/##MESSAGE##/WARNING: SQL Query returned $results. for stored procedure $opt_proc/g;
-	print $indexFile;
-	exit 1;
+sub print_ok($results) {
+    $table =~ s/##LABEL##/ok/g;
+    $table =~ s/##TYPE##/OK/g;
+    
+    print $table;
+    exit 0;
 }
 
-sub print_critical($results, $indexFile) {
-	$footer = '<div class="box-row footer"><table><tr><td>CRITICAL: SQL Query returned $results. for stored procedure $opt_proc. The critical threshold is $opt_c.</td></tr></table></div>';
-	$indexFile =~ s/##CONTENT##/$header$table$footer/g;
-	$indexFile =~ s/##TYPE##/erro/g;
-	$indexFile =~ s/##MESSAGE##/CRITICAL: SQL Query returned $results. for stored procedure $opt_proc/g;
-	print $indexFile;
-	exit 2;
+sub print_warning($results) {
+    $table =~ s/##LABEL##/aviso/g;
+    $table =~ s/##TYPE##/WARNING/g;
+
+    print $table;
+    exit 1;
 }
+
+sub print_critical($results) {
+    $table =~ s/##LABEL##/erro/g;
+    $table =~ s/##TYPE##/CRITICAL/g;
+
+    print $table;
+    exit 2;
+}
+
 
 sub print_help {
     print "Usage: $0 -H HOSTNAME -p PROCEDURE -d database -u user -P password -w <warn> -c <crit>\n";
@@ -205,21 +205,21 @@ sub print_help {
     This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 -p, --procedure
-	Stored procedure to execute
+        Stored procedure to execute
 -H, --hostname
-	Hostname of database server
+        Hostname of database server
 -d  --database
-	Database to run Stored Procedure against
+        Database to run Stored Procedure against
 -u, --user
     SQL Username
 -P, --password
     SQL Password
 -c, --critical
-	Value at which when met or exceeded will send a critical alert
+        Value at which when met or exceeded will send a critical alert
 -w, --warning
-	Value at which when met or exceeded (but is lower then critical value) will send a warning alert.
+        Value at which when met or exceeded (but is lower then critical value) will send a warning alert.
 -h, --help
-	Display detailed help
+        Display detailed help
 -v, --version
     Show version information
 
@@ -233,15 +233,14 @@ sub print_help {
     not be visible from the web console or 2. If you have a universal SQL login for all of your Nagios queries, then
     you may hardcode the username & password into the beginning of this script.
 
+
     Send email to jeremy\@pavleck.com or nagios-users\@lists.sourceforge.net if you have questions regarding the use of this
-    software. To submit patches or suggest improvements, please email jeremy\@pavleck.com or visit www.Pavleck.com
-";
+    software. To submit patches or suggest improvements, please email jeremy\@pavleck.com or visit www.Pavleck.com";
 }
 
 sub print_version {
-	print "
-	$0  version 1.01 - October 28th, 2005
+    print "
+    $0  version 1.01 - October 28th, 2005
     Copyright (c) 2005 Jeremy D. Pavleck <jeremy\@pavleck.com>
-    This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
-    ";
+    This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.";
 }
